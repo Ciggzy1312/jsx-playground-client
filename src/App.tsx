b1b0578b2e0c634aa9d6f93f8c8c5492d61a05ce
Deepayan <deepayanmukherjee1312@gmail.com>
2022-02-02 10:45:49 +0530

    Added basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState("")
+
+  const [code, setCode] = useState("")
+
+  const handler = ()=>{
+    console.log(input)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea value={input} onChange={e=>setInput(e.target.value)}></textarea>
+
+      <button onClick={handler}>Submit</button>
+
+      <pre>{code}</pre>
     </div>
   );
 }
