2adb7da2e66f574f6bc0d0ea06f954498c9958db
Deepayan <deepayanmukherjee1312@gmail.com>
2022-02-03 03:43:54 +0530

    Transpiling and bundling works using esbuild-wasm and unpkg respectively

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 function App() {
 
-  const [input, setInput] = useState("")
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-  const [code, setCode] = useState("")
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-  const handler = ()=>{
-    console.log(input)
-  }
+  const handler = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  };
+
   return (
     <div className="App">
       <textarea value={input} onChange={e=>setInput(e.target.value)}></textarea>
