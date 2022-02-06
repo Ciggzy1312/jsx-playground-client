b6150be8aa2de396cc14185b1da2e17ada3ab7c0
Deepayan <deepayanmukherjee1312@gmail.com>
2022-02-06 14:45:30 +0530

    Implemented caching

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,22 +1,29 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios'
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+});
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-
+        console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
         }
 
-        if (args.path.includes('./') || args.path.includes('../')){
+        if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL( args.path,'https://unpkg.com' + args.resolveDir + '/' ).href,
-          }
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
+          };
         }
 
         return {
@@ -27,24 +34,34 @@ export const unpkgPathPlugin = () => {
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
-        
+
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import React, { useState } from 'react';
+              import React, { useState } from 'react-select';
               console.log(React, useState);
             `,
           };
-        } 
-        
+        }
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname
+          resolveDir: new URL('./', request.responseURL).pathname,
         };
+        await fileCache.setItem(args.path, result);
 
+        return result;
       });
     },
   };
