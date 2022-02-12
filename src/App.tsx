5c3bfcd8cd231608d1fdae759bfaaf3148734fe7
Deepayan <deepayanmukherjee1312@gmail.com>
2022-02-12 18:35:31 +0530

    Handled multiple editors and previews

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
-import { useState } from 'react';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundler from './bundler';
-
+import CodeCell from './components/code-cell';
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
 function App() {
 
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const handler = async () => {
-    
-    const output = await bundler(input);
-
-    setCode(output);
-  };
-
   return (
     <div>
-
-      <CodeEditor initialValue="const a = 1;" onChange={(value) => setInput(value)}/>
-
-      <div>
-        <button onClick={handler}>Submit</button>
-      </div>
-
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 }
