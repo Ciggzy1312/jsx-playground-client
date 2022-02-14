f9362c03671d1acfdb9c82a4b6b469bb46386946
Deepayan <deepayanmukherjee1312@gmail.com>
2022-02-14 23:35:35 +0530

    Fixed resizing performance and bugs

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
@@ -7,21 +8,50 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 
+  const [ width, setWidth] = useState(window.innerWidth * 0.75)
+  const [ innerWidth, setInnerWidth] = useState(window.innerWidth)
+  const [ innerHeight, setInnerHeight] = useState(window.innerHeight)
+
   let resizableProps: ResizableBoxProps;
+
+  useEffect(()=>{
+    let timer:any
+    const listener = ()=>{
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+      },100)
+    }
+
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  },[width])
 
   if (direction === 'horizontal') {
     resizableProps = {
       className: 'resize-horizontal',
-      minConstraints: [window.innerWidth * 0.25, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.25, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width,
       resizeHandles: ['e'],
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
       minConstraints: [Infinity, 50],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       height: 300,
       width: Infinity,
       resizeHandles: ['s'],
