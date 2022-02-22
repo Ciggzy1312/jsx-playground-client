93433f7e54a11b8797afce803d54a2c49c617561
Deepayan <deepayanmukherjee1312@gmail.com>
2022-02-22 11:48:29 +0530

    Added action bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import CellList from './components/cell-list';
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 function App() {
 
